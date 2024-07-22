5fe12d9deeb4a9c9fb0699c1a4b0c30d2bcea546
vermaji9045 <59643845+vermaji9045@users.noreply.github.com>
2024-07-22 10:33:45 +0530

    Authentication and Authorization

--- gatewayserver/gatewayserver/src/main/java/com/bank/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/gatewayserver/src/main/java/com/bank/gatewayserver/config/SecurityConfig.java
@@ -4,12 +4,18 @@ package com.bank.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.event.AbstractAuthenticationEvent;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
+import reactor.core.publisher.Mono;
 
 
 @Configuration
@@ -19,14 +25,22 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
-                        .pathMatchers("/publicbank/accounts/**").authenticated()
-                        .pathMatchers("/publicbank/cards/**").authenticated()
-                        .pathMatchers("/publicbank/loans/**").authenticated())
+                        .pathMatchers("/publicbank/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/publicbank/cards/**").hasRole("CARDS")
+                        .pathMatchers("/publicbank/loans/**").hasRole("LOANS"))
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                        .jwt(Customizer.withDefaults()));
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExecutor())));
         serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
         return serverHttpSecurity.build();
     }
 
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExecutor()
+    {
+        JwtAuthenticationConverter jwtAuthenticationConverter=new JwtAuthenticationConverter();
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
 
 }
